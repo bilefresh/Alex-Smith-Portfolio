515c976e3a98e5ea3c18364dec2d6937d7f6cd71
Bilesanmi farouk <bilesanmifaruk@gmail.com>
2023-10-03 22:36:37 +0100

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="Alex Smith - UI/UX Designer"
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Alex Smith - UI/UX Designer"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/img/favicon.ico" />
